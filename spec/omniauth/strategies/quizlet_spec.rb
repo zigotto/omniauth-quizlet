8b670999e5a93a8e93baf8c3d055ecb838182099
Jesus Lopes <jlopes@zigotto.com.br>
2012-06-17 22:29:07 -0300

    Added some test for client options

---
+++ spec/omniauth/strategies/quizlet_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe OmniAuth::Strategies::Quizlet do
+
+  subject do
+    OmniAuth::Strategies::Quizlet.new({})
+  end
+
+  context "client options" do
+    it "should have correct site" do
+      subject.options.client_options.site.should eq("https://api.quizlet.com")
+    end
+
+    it "should have correct authorize url" do
+      subject.options.client_options.authorize_url.should eq('https://quizlet.com/authorize/')
+    end
+
+    it "should have correct token url" do
+      subject.options.client_options.token_url.should eq('https://api.quizlet.com/oauth/token')
+    end
+  end
+
+end
